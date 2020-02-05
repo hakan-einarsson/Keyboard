aedfa6b3e8be6ee81e5697a430743ba3c5b00b70
Håkan Einarsson <hakan.einarsson@xpdo.xpeedio.local>
2020-02-05 13:51:47 +0100

    its working

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const store = new Vuex.Store({
     state: {
-        oscillators: [{id:0,wave:"sine",filter: "bandpass", volume: 0.5,volume_start:0, volume_end:0.5,octave:1,frequency: 50, length:1},],
+        oscillators: [{id:0,wave:"sine",filter: "bandpass", volume: 0.1,volume_start:0.5, volume_end:0.5,octave:1,frequency: 50, length:1},],
         notes: {"a":440,
                 "bs":440*Math.pow(2,1/12),
                 "b":440*Math.pow(2,2/12),
@@ -12,8 +12,26 @@ const store = new Vuex.Store({
                 "f":440*Math.pow(2,8/12),
                 "fs":440*Math.pow(2,9/12),
                 "g":440*Math.pow(2,10/12),
-                "gs":440*Math.pow(2,11/12)}
+                "gs":440*Math.pow(2,11/12)},
+        ctx : new AudioContext()
         },
+    mutations: {
+        addOsc: state => {
+            state.oscillators.push({id:store.getters.getOsc.length,wave:"sine",filter: "bandpass", volume: 0.1,volume_start:0.5, volume_end:0.5,octave:1,frequency: 50, length:1})
+        },
+        changeProperty: function(state,values){
+            state.oscillators[values.id][values.name] = values.property
+        },
+        removeOsc: function(state, id){
+            if (store.getters.getOsc.length > 1){
+            state.oscillators.splice(id,1)
+                for (let index = 0; index < store.getters.getOsc.length; index++) {
+                    store.dispatch('changeProperty',{id:index, name:"id",property:index})
+                }
+                
+            }
+        }
+    },
  
     getters: {
         returnNote: function(state){
@@ -21,13 +39,26 @@ const store = new Vuex.Store({
         },
         getOsc: function(state){
             return state.oscillators
+        },
+        getCtx: function(state) {
+            return state.ctx
+        }
+    },
+    actions: {
+        addOsc: function(context){
+            context.commit('addOsc')
+        },
+        changeProperty: function(context,values){
+            context.commit('changeProperty', values)
+        },
+        removeOsc: function(context, id){
+            context.commit('removeOsc',id)
         }
     }        
 })
 
 Vue.component('oscillator',{
     props: ["item"],
-    
     template:'<div class="row justify-content-center"><div class="col-1 text-center">{{this.item.id+1}}</div>\
                 <div class="col-1"><select class="form-control" v-model:value=wave>\
                 <option value="sine">Sine</option><option value="square">Square</option>\
@@ -42,14 +73,14 @@ Vue.component('oscillator',{
                 <div class="col-1"><input type="text" class="form-control" v-model:value=volume_start></div>\
                 <div class="col-1"><input type="number" class="form-control" v-model:value=octave></div>\
                 <div class="col-1"><input type="number" class="form-control" v-model:value=frequency></div>\
-                <div class="col-1"><input type="number" step="0.01" class="form-control" v-model:value=length></div></div>',
+                <div class="col-1"><input type="number" step="1" class="form-control" v-model:value=length></div><div class="col-1"><button class="btn btn-danger" @click=remove>Remove</button></div></div>',
     computed: {
         wave: {
             get(){
                 return this.$store.state.oscillators[this.item.id].wave
             },
             set(value){
-                return "value"
+                this.$store.dispatch('changeProperty',{id:this.item.id,name:"wave",property:value})
             }
         },
         filter: {
@@ -57,7 +88,7 @@ Vue.component('oscillator',{
                 return this.$store.state.oscillators[this.item.id].filter
             },
             set(value){
-                return "value"
+                this.$store.dispatch('changeProperty',{id:this.item.id,name:"filter",property:value})
             }
         },
         volume: {
@@ -65,7 +96,7 @@ Vue.component('oscillator',{
                 return this.$store.state.oscillators[this.item.id].volume
             },
             set(value){
-                return "value"
+                this.$store.dispatch('changeProperty',{id:this.item.id,name:"volume",property:parseFloat(value)})
             }
         },
         volume_start: {
@@ -73,7 +104,7 @@ Vue.component('oscillator',{
                 return this.$store.state.oscillators[this.item.id].volume_start
             },
             set(value){
-                return "value"
+                this.$store.dispatch('changeProperty',{id:this.item.id,name:"volume_start",property:parseFloat(value)})
             }
         },
         volume_end: {
@@ -81,7 +112,7 @@ Vue.component('oscillator',{
                 return this.$store.state.oscillators[this.item.id].volume_end
             },
             set(value){
-                return "value"
+                this.$store.dispatch('changeProperty',{id:this.item.id,name:"volume_end",property:parseFloat(value)})
             }
         },
         octave: {
@@ -89,7 +120,7 @@ Vue.component('oscillator',{
                 return this.$store.state.oscillators[this.item.id].octave
             },
             set(value){
-                return "value"
+                this.$store.dispatch('changeProperty',{id:this.item.id,name:"octave",property:parseInt(value)})
             }
         },
         frequency: {
@@ -97,7 +128,7 @@ Vue.component('oscillator',{
                 return this.$store.state.oscillators[this.item.id].frequency
             },
             set(value){
-                return "value"
+                this.$store.dispatch('changeProperty',{id:this.item.id,name:"frequency",property:parseInt(value)})
             }
         },
         length: {
@@ -105,10 +136,15 @@ Vue.component('oscillator',{
                 return this.$store.state.oscillators[this.item.id].length
             },
             set(value){
-                return "value"
+                this.$store.dispatch('changeProperty',{id:this.item.id,name:"length",property:parseInt(value)})
             }
         },
         
+    },
+    methods: {
+        remove: function(){
+            this.$store.commit('removeOsc',this.item.id)
+        }
     }
 });
 
@@ -117,31 +153,34 @@ const app = new Vue({
     store,
     methods : {
         playNote: function (note){
-            let ctx = new AudioContext();
-            let filter=ctx.createBiquadFilter();
-            let oscillator = ctx.createOscillator();
-            let gainNode = ctx.createGain();
-            filter.connect(gainNode);
-            gainNode.connect(ctx.destination);
-            oscillator.connect(filter);
-            filter.type="bandpass";
-            filter.frequency.value=70;
-            gainNode.gain.setValueAtTime(0.1,ctx.currentTime);
-            gainNode.gain.setTargetAtTime(0,ctx.currentTime,0.4);
-            oscillator.type="sine";
-            oscillator.frequency.setValueAtTime(note,ctx.currentTime);
-            oscillator.start();
-            oscillator.stop(ctx.currentTime+2);
+            this.$store.getters.getOsc.forEach(e => {
+                let baseNote = note.split("-")[0]
+                let octave = note.split("-")[1]
+                let frequency = this.$store.getters.returnNote[baseNote]*1/Math.pow(2,(1-octave))
+                let filter=this.$store.getters.getCtx.createBiquadFilter();
+                let oscillator = this.$store.getters.getCtx.createOscillator();
+                let gainNode = this.$store.getters.getCtx.createGain();
+                filter.connect(gainNode);
+                gainNode.connect(this.$store.getters.getCtx.destination);
+                oscillator.connect(filter);
+                filter.type=e.filter;
+                filter.frequency.value=e.frequency;
+                gainNode.gain.setValueAtTime(e.volume,this.$store.getters.getCtx.currentTime);
+                gainNode.gain.setTargetAtTime(0,this.$store.getters.getCtx.currentTime,e.volume_start);
+                oscillator.type=e.wave;
+                oscillator.frequency.setValueAtTime(frequency,this.$store.getters.getCtx.currentTime);
+                oscillator.start();
+                oscillator.stop(this.$store.getters.getCtx.currentTime+e.length);
+            });
         
         },
         getNote: function(note){
             let baseNote = note.split("-")[0]
             let octave = note.split("-")[1]
             this.playNote(this.$store.getters.returnNote[baseNote]*1/Math.pow(2,(1-octave)));
-            console.log(this.$store.getters.returnNote[baseNote]*1/Math.pow(2,(1-octave)))
         },
         test: function(){
-            console.log(this.$store.getters.returnNote['c'])
+            store.dispatch('addOsc')
         }
 
     }
